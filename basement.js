452d45f050a6c36907d185dfa74a8ab6b0525988
nzonbi <daniel@ozkeebo.com>
2011-11-03 19:45:17 -0430

    v0.8 working

--- basement.js
+++ basement.js
@@ -1,143 +1,127 @@
 //
 // basement.js
 //
-// javascript mini testing lab
+// javascript testing lab
 //
 var basement = function() {
 
-	// o public space
-	
-	o = {}
-	
-	
-	
-	o.html = null
+
+	// public
+	pub = {}
 	
 	
+	// counters
+	var tests_count = 0
+	var passed_tests = 0
+	var failed_tests = 0
+
+
+	// starts a new set of tests
+	pub.set = function( title ) {
+
+	}
 	
-	rgx_name = /^\w[\w\d_]*$/
+	
+	
 	
 	// create a new test
-	o.test = function( name ) {
+	// return the test object
+	pub.new = function( name, descrip ) {
+				
+		tests_count++
+		passed_tests++
 		
-		console.log( 'new test: ' + name )
-			
-		if( ! rgx_name.test( name ) )
-			throw "Invalid test name. Start with a letter, and only letters or numbers, no spaces"
 		
+		// create the test object
 		var test = Object.create( proto_test )
+		// sets test id
+		test.id = tests_count
+		// sets name
 		test.name = name
-		test.tests = []
+		// sets the description
+		if(descrip)
+			test.descrip = descrip
+		else
+			test.descrip = ""
+		// creates a liust for the checks results
+		test.checks = []
 		
-		add_test_label( name )
+		view.add_test( test )
 		
+		// return the test object
 		return test
 	}
 	
 	
 	
-
-	
-	
-	
-	var html_test = '<div id="{name}" class="test_top">{name}</div>'
-	
-	var html_item = '<div id="{name}" class="test_item">{res} {name}</div>'
-	var html_item_ok = '<span class="ok">ok</span>'
-	var html_item_fail = '<span class="fail">fail</span>'
-	
-	
-	var div
-	
-	var add_test_label = function( label ) {
-		
-		var html = html_test
-			.replace( '{name}', label )
-			.replace( '{name}', label )
-		
-		div = $( html ).appendTo( '#' + o.html )
-	}
-
-
-
-
-	var add_sub_label = function( label, bool ) {
-				
-		var html = html_item
-			.replace( "{name}", label )
-			.replace( "{name}", label )
-		
-		if( bool ) 
-			html = html.replace( '{res}', html_item_ok )
-		else
-			html = html.replace( '{res}', html_item_fail )
-			
-		$( html ).appendTo( '#' + o.html )
-	}	
-		
-		
-		
-	// add a test result to the test object
-	var	test = function( label, bool ) { 
-
-		add_sub_label( label, bool )
-		this.tests.push( bool )
-		
-		return this 
-	}
-		
-	
-		
-	
-	var done = function() {
-		
-		console.log( 'done' )
-		
-		if( this.tests.length == 0 ) {
-			this.pass = false
-			return false
-		}
-	
-		
-		this.pass = mix_tests( this.tests )
-		
-		console.log( "   pass: " + this.pass )	
-			
-		
-		if( this.pass ) {
-			$( '#' + this.name ).addClass('test_top_ok')
-			console.log( 'class test_top_ok added to ' + this.name )
-		} else {
-			$( '#' + this.name ).addClass('test_top_fail')
-			console.log( 'class test_top_fail added to ' + this.name )
-		}
-				
-	}
-	
+	var current_test
 
 
 	// test object prototype
 	var proto_test = {
 		
+		// id
+		id: null,
+		
+		// test name
 		name: '',
-		pass: null,
-		notes: '',
-		test: test,
-		done: done
+		
+		// test passed status, true of false
+		pass: true,
+		
+		// test description
+		descrip: '',
+		
+		// performs a check within the test,
+		// and updates the view with the result
+		check: function( label, bool, descrip ) { 
+						
+			this.checks.push( bool )
+			view.add_check( label, bool, descrip )
+			
+			this.update_status( bool )
+			return this 
+		},
+			
+
+		update_status: function( bool ) {
+			
+			// determine if any counter should be increased
+			console.log('====================================')
+			console.log( 'bool: ' + bool )
+			console.log( 'this.pass: ' + this.pass )
+			if( this.pass && !bool ) {
+				console.log('changed')
+				passed_tests--
+				failed_tests++					
+			}
+			
+			console.log( 'passed_tests: ' + passed_tests )
+			console.log( 'failed_tests: ' + failed_tests )
+			
+			view.update_counters()
+
+			this.pass = this.pass && bool
+
+			// update the view
+			view.set_test_pass_status( this )
+					
+		}
 		
 	}
+	// end of test object prototype
 	
 	
 	
 	
 	// tests if all values in an array are true
+	// used to verify if al checks in a test were passed
 	var mix_tests = function( list ) {
 
 		var res = true, i = 0
 			
 		for (i=0; i<list.length; i++) {
 			res = res && list[i]
-			console.log('      mix tests - res: ' + res )
 		}
 		
 		return res
@@ -145,9 +129,103 @@ var basement = function() {
 
 
 
+	
+	/////////////////////////////////////////////////////////////	
+	/////////////////////////////////////////////////////////////
+	// html view
+	
+	var view = {}
+	
+	
+	// view setup
+	// sets the test button event
+	view.setup = function() {	 
+		$('#test_button').click( test.do );
+	}
+	$( window ).load( view.setup )
+	
+	
+	
+	// html element where the test fields will be show
+	view.tests_box = '#tests_box'
+
+
+	// views 
+	view.test = '<div>'
+	+ '<div class="test_id">{test.id}</div>'
+	+ '<div id="{test.id}" class="test">{test.name}'
+	+ '<div class="descrip">{test.descrip}</div>'
+	+ '</div>'
+	+ '</div>'
+	
+	view.check = '<div id="{name}" class="check">{res} <div class="check_text">{name}</div></div>'
+	view.check_ok = '<div class="ok">ok</div>'
+	view.check_fail = '<div class="fail">fail</div>'
+	
+	
 
 
 	
-	return o
+	// add a test to the view
+	view.add_test = function( test ) {
+				
+		var html = view.test
+			.replace( '{test.id}', test.id )
+			.replace( '{test.id}', test.id )
+			.replace( '{test.name}', test.name )
+			.replace( '{test.descrip}', test.descrip )
+		
+		$( html ).appendTo( view.tests_box )
+	}
+
+	
+
+	// set test pass statuss
+	view.set_test_pass_status = function( test ) {
+		
+		$( '#' + test.id ).removeClass('ok')
+		$( '#' + test.id ).removeClass('fail')
+		
+		if( test.pass ) {
+			$( '#' + test.id ).addClass('ok')
+		} else {
+			$( '#' + test.id ).addClass('fail')
+		}		
+		
+	}
+
+
+	// add a check to the view
+	view.add_check = function( label, bool ) {
+				
+		var html = view.check
+			.replace( "{name}", label )
+			.replace( "{name}", label )
+		
+		if( bool ) 
+			html = html.replace( '{res}', view.check_ok )
+		else
+			html = html.replace( '{res}', view.check_fail )
+			
+		$( html ).appendTo( view.tests_box )
+	}
+	
+	
+	view.update_counters = function() {
+		
+		$('#tests_passed').text( passed_tests )
+		$('#tests_failed').text( failed_tests )
+		
+	}	
+		
+		
+
+	/////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////	
+	
+	
+	
+	return pub
 	
 }()
+
--- readme.md
+++ readme.md
@@ -1,11 +1,37 @@
 
 # basement.js
-v0.01
+v0.8
 
-javascript testing mini framework
+javascript testing lab
 
 
-* documentation and some changes pending
+basament is a simple testing library for javascript, with an html view.
+
+## Usage
+
+Example:
+
+basament.set( "a new set of tests" )
+
+basament.new( "the first test" )
+	.check( "check 1", a == b )
+	.check( "check 2", c == a + b )
+	.check( "check 3", d == a + b + c )
+
+The results of the checks for each test is reported in the html. If all checks are passed, the test is passed.
+
+
+### The basament module, provides two methods:
+
+* basament.set( name )	start a new set of tests, and sets the title to name.
+
+* basament.new( name )	create and return a new -basament test object-
+
+
+### The -basament test object- has only one method:
+
+* basament-test-object.check( name, check-boolean )	performs a check. if "check-boolean" is true, the check is passed.
+
 
 
 
